906d5421d089c65a0aa7ce2e66e55613df95d5ba
IlCarretto <carlorebuffat412@gmail.com>
2022-11-14 12:49:50 +0100

    done first milestone

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,31 @@
+<script>
+import {store} from "../store";
+
+export default {
+    name: "MovieCard",
+    props: {
+        movie: Object
+    }
+}
+</script>
+
+<template>
+    <div class="ms-card">
+        <div class="top-card">
+
+        </div>
+        <div class="bottom-card">
+            <h2>{{movie.title}}</h2>
+            <h3>{{movie.original_title}}</h3>
+            <p>{{movie.original_language}}</p>
+            <p>{{movie.vote_average}}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .ms-card {
+        border: 1px solid black;
+        min-height: 250px;
+    }
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,17 +1,32 @@
 <script>
+import {store} from "../store";
+import MovieCard from "./MovieCard.vue";
+
 export default {
     name: "MoviesList",
+    components: {
+        MovieCard
+    },
     data() {
         return {
-
+            store
         }
     }
 }
 </script>
 
 <template>
-a
+    <main>
+        <div class="container">
+            <div class="row row-cols-5 g-4">
+                <div v-for="(movie, index) in store.movies" class="col">
+                    <MovieCard :movie="movie"/>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+
 </style>
